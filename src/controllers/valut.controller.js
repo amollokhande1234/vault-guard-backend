afee43df66ec53e2907e747699c7bb0d42e1f2c1
Amol Lokhande <lokhandeamol0000@gmail.com>
2026-05-27 00:18:24 +0530

    Encryption Done

--- src/controllers/valut.controller.js
+++ src/controllers/valut.controller.js
@@ -267,16 +267,23 @@ const getSingleVault = async (req, res) => {
 
         const now = new Date();
 
-        if (now < vault.unlockDate || vault.status === "locked") {
+
+        if (now < vault.unlockDate) {
             return res.status(403).json({
                 success: false,
                 message: "Vault is still locked",
             });
         }
 
+        if (vault.status === "locked") {
+            vault.status = "unlocked";
+            vault.isUnlocked = true;
+            await vault.save();
+        }
+
         // unlock update (safe)
         vault.isUnlocked = true;
-        vault.status = "unlocked"; z
+        vault.status = "unlocked";
         await vault.save();
 
         return res.status(200).json({
