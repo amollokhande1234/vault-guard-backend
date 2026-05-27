4c21b8ec6d782e20cae5feaa4c219122f44ae6d3
Amol Lokhande <lokhandeamol0000@gmail.com>
2026-05-27 09:44:53 +0530

    OTP added fun

--- src/config/nodemailer.js
+++ src/config/nodemailer.js
@@ -1,7 +1,10 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    // service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,  // ← App Password here
