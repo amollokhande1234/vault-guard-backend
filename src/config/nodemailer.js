8ba7cbd39fca3f85f397e875a98112ed95ac1a72
Amol Lokhande <lokhandeamol0000@gmail.com>
2026-05-27 20:30:14 +0530

    OTP added fun ahjad

--- src/config/nodemailer.js
+++ src/config/nodemailer.js
@@ -1,10 +1,22 @@
 const nodemailer = require("nodemailer");
 
+// const transporter = nodemailer.createTransport({
+//     service: "gmail",
+//     auth: {
+//         user: process.env.EMAIL_USER,
+//         pass: process.env.EMAIL_PASS, // App Password only
+//     },
+// });
+
+
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,           // use 587 (TLS) instead of 465 (SSL)
+    secure: false,       // true for 465, false for 587
+    family: 4,           // 👈 force IPv4
     auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS, // App Password only
+        pass: process.env.EMAIL_PASS,
     },
 });
 module.exports = transporter;
